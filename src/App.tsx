bff62a898cb7b1d96830138c3914394a2e43b57d
boatnoah <noahskim7@gmail.com>
2024-07-11 18:50:05 -0700

    feat - created CRUD routes for content creators

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Card from './components/Card';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Card />} />
+        <Route path="/" />
         <Route path="/" >
         </Route>
       </Routes>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,14 @@
+interface CardInfo {
+  name: string,
+  description: string,
+};
 
-const Card = () => {
+const Card = ({ name, description }: CardInfo) => {
   return (
     <div className="flex flex-col border border-black m-10 p-10 w-1/5 h-72 rounded-lg bg-gray-100">
-      <h1 className="text-3xl font-bold">Name</h1>
+      <h1 className="text-3xl font-bold">{name || "Name"}</h1>
       <h2 className="text-lg">Description</h2>
-      <p>......................</p>
+      <p>{description}</p>
       <button className="border border-black rounded-lg">Edit</button>
     </div>
   )
