316b39c958ed988a276c35edf4397d3f60ae46b4
boatnoah <noahskim7@gmail.com>
2024-07-11 18:42:06 -0700

    feat - implemented card components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Card from './components/Card';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<Card />} />
         <Route path="/" >
         </Route>
       </Routes>
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <div className="flex flex-col border border-black m-10 p-10 w-1/5 h-72 rounded-lg bg-gray-100">
+      <h1 className="text-3xl font-bold">Name</h1>
+      <h2 className="text-lg">Description</h2>
+      <p>......................</p>
+      <button className="border border-black rounded-lg">Edit</button>
+    </div>
   )
 }
 
